f3a323efd00cc06f43f869c04d7640bbf02e393c
sean darsie <seandarsie@seans-MacBook-Air.local>
2017-07-08 15:28:08 -0700

    Game entity update

--- src/GameEntity.class.cpp
+++ src/GameEntity.class.cpp
@@ -1,21 +1,39 @@
-#include "GameEntity.hpp"
+#include "GameEntity.class.hpp"
 
 GameEntity::GameEntity(void)
 {
-  this->_x;
-  this->_y;
-  this->_speed;
-  this->_symbol;
-  this->_lives;
-  this->_isAlive;
+  this->_x = 0;
+  this->_y = 0;
+  this->_speed = 1;
+  this->_dirx = 1;
+  this->_diry = 1;  
+  this->_symbol = '#';
+  this->_lives = 1;
+  this->_isAlive = true;
 }
 
-GameEntity::GameEntity(std::string)
+GameEntity::GameEntity(std::string name)
 {
+  this->_x = 0;
+  this->_y = 0;
+  this->_speed = 1;
+  this->_dirx = 1;
+  this->_diry = 1;  
+  this->_symbol = '#';
+  this->_lives = 1;
+  this->_isAlive = true;
 }
 
 GameEntity::GameEntity(GameEntity &src)
 {
+  this->_x = src._x;
+  this->_y = src._y;
+  this->_speed = src._speed;
+  this->_dirx = src._dirx;
+  this->_diry = src._diry;  
+  this->_symbol = src._symbol;
+  this->_lives = src._lives;
+  this->_isAlive = src._isAlive;
 }
 
 GameEntity::~GameEntity(void)
@@ -28,8 +46,23 @@ bool GameEntity::move(int x, int y)
 {
   std::cout << "Destructor called" << std::endl;
   // if movement results in a collision return false and kill entity;
-  if (this->_x + x is occupied)
+  if (this->_x + x >= maxX)
+    {
+      mvwprintw(Game::_debugwin, x, y, "%s\n", "collision detected");
+      return (false);
+    }
+  else if (this->_y + y >= maxY)
     return (false);
   return(true);
 }
   
+
+void GameEntity::drawToWindow(int x, int y) const
+{
+  // Draw this->GameEntity at it's x and y after it has moved or not
+}
+
+void GameEntity::getAlive(void) const
+{
+  return (this->_isAlive);
+}
