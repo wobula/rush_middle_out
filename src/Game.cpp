663feaa15ff8f5f756b70919b001b55a27084481
Brett Merrill <brett.j.merrill94@gmail.com>
2017-07-08 15:01:57 -0700

    Game object files

---
+++ src/Game.cpp
@@ -0,0 +1,28 @@
+#include "Game.hpp"
+#include <ncurses.h>
+
+int score, maxY, maxX;
+Game()
+{
+};
+
+Game(const Game &other)
+{
+};
+
+virtual ~Game() noexcept
+{
+};
+
+Game &operator=(const Game &other)
+{
+};
+
+void launch()
+{
+
+};
+void play()
+{
+
+};
