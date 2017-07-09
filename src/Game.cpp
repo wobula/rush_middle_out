e173c50f567f5383d8828e7e50bdbe97810a3356
Isaac <isaacjrhett@gmail.com>
2017-07-08 18:12:34 -0700

    added pseudocode

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,15 +4,15 @@
 int score, maxY, maxX;
 Game::Game()
 {
-  score = 0;
-  maxX = 0;
-  maxY = 0;
-  debug = false;
+		score = 0;
+		maxX = 0;
+		maxY = 0;
+		debug = false;
 };
 
 Game::Game(const Game &other)
 {
-  *this = other;
+		*this = other;
 };
 
 Game::~Game()
@@ -21,35 +21,55 @@ Game::~Game()
 
 Game& Game::operator=(const Game &rhs)
 {
-  // Check for self-assignment!
-  if (this == &rhs)
-    return *this;
+		// Check for self-assignment!
+		if (this == &rhs)
+				return *this;
 
-  return *this;
+		return *this;
 }
 
 void Game::launch()
 {
-  initscr();
-  cbreak();
-  noecho();
-  keypad(stdscr, TRUE);
-  getmaxyx(stdscr, maxY, maxX);
-  printw("window size id %d tall and %d wide", maxY, maxX);
-  play();
-  endwin();
+		initscr();
+		cbreak();
+		noecho();
+		keypad(stdscr, TRUE);
+		getmaxyx(stdscr, maxY, maxX);
+		printw("window size id %d tall and %d wide", maxY, maxX);
+		play();
+		endwin();
 };
 void Game::play()
 {
-  int ch = 0;
-  timeout(50);
-  while ((ch = getch()) != 'q')
-    {
-      if (ch == 'D')
-        debug = true;
-      if (ch != ERR)
-        addch(ch);
-      refresh();
-      ch = 0;
-    }
+		int ch = 0;
+		timeout(50);
+		while ((ch = getch()) != 'q')
+		{
+				// This Game will have an array of bullets. 
+				// This function just needs to loop through the array and move each bullet once,
+				// check for collisions and change is_alive as needed
+				this.bulletsAct();
+
+				// Do what for and mean to be. Fire lasers, do some damage! Move the player!
+				this.playerAct(ch);
+
+				// Loop through the array of Enemies and have them move, shoot, etc.
+				this.enemiesAct();
+
+				// every X cycles, spawn a new enemy at a random position on the spawn wall.
+				this.spawnEnemies();
+
+				// loop through all enemies, bullets, and players, and remove all entities with 
+				// is_alive == false;
+				this.cleanup();
+
+
+
+				if (ch == 'D')
+						debug = true;
+				if (ch != ERR)
+						addch(ch);
+				refresh();
+				ch = 0;
+		}
 };
