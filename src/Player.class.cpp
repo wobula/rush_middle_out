ab35b89a5a6773b9385a50838f0176195bad3167
Tony Hendrick <thendrick@Tony-Hendricks-MacBook-Pro.local>
2017-07-08 17:28:48 -0700

    Player move and shoot

--- src/Player.class.cpp
+++ src/Player.class.cpp
@@ -10,7 +10,27 @@
 
 #include <iostream>
 #include <string>
+#include <curses.h>
 #include "Player.class.hpp"
+
+// METHODS //
+
+void Player::shoot(std::string gun)
+{
+	Missile::_instantiate(this->_x + 1, this->_y + 1);
+}
+
+bool Player::move(int x, int y)
+{
+	if (KEY_UP)
+		this->_x = this->_x + 1;
+	if (KEY_DOWN)
+		this->_x = this->_x -1;
+	if (KEY_LEFT)
+		this->_y = this->_y + 1;
+	if (KEY_RIGHT)
+		this->_y this->_y - 1;
+}
 
 // OPERATOR OVERLOADS //
 
@@ -35,16 +55,22 @@ Player::Player(std::string name)
 	std::srand(std::time(NULL));
 	this->_initValue();
 	this->_name = name;
-	std::cout << "Player " << this->_name
-		<< " has been created!" << std::endl;
+	if (Game::debug)
+	{
+		std::cout << "Player " << this->_name
+			<< " has been created!" << std::endl;
+	}
 }
 
 Player::Player(Player const & src)
 {
 	std::srand(std::time(NULL));
 	*this = src;
-	std::cout << "Player " << this->_name
-		<< " has been created!" << std::endl;
+	if (Game::_debug)
+	{
+		std::cout << "Player " << this->_name
+			<< " has been created!" << std::endl;
+	}
 	return;
 }
 
@@ -53,14 +79,14 @@ Player::Player(void)
 	std::srand(std::time(NULL));
 	this->_initValue();
 	//std::cout << "Player has been created!" << std::endl;
-	mvwprintw(Game::_debugwin, GAME::_maxX / 2, GAME::_maxY / 2, "Player has been created!");
+	mvwprintw(Game::_stdscr, GAME::_maxX / 2, GAME::_maxY / 2, "Player has been created!");
 }
 
 // DECONSTRUCTORS //
 
 Player::~Player(void)
 {
-	mvwprintw(GAME::_debugwin, GAME::_maxX / 2, GAME::_maxY / 2, "Player has been destroyed!");
+	mvwprintw(GAME::_stdsrc, GAME::_maxX / 2, GAME::_maxY / 2, "Player has been destroyed!");
 	return;
 }
 
